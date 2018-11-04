52668dd3f6cd756772f5171e28779473f22205cb
WRindone <wrindone268@gmail.com>
2018-11-04 17:59:57 -0600

    Potentially got the arrow game working

---
+++ Alt-phys/Assets/BowArrow/Scripts/Arrow.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This code is more or less completely made by owner of FusedVR youtube channel
+//Updated by William Rindone to work with the Oculus SDK
+
+public class Arrow : MonoBehaviour {
+
+    private bool isAttached = false;
+
+    private bool isFired = false;
+
+    void OnTriggerStay()
+    {
+            AttachArrow();
+    }
+
+    void OnTriggerEnter()
+    {
+            AttachArrow();
+    }
+
+    private void Update()
+    {
+        if(isFired){
+            transform.LookAt(transform.position + transform.GetComponent<Rigidbody>().velocity);
+        }
+    }
+
+    public void Fired(){
+        isFired = true;
+    }
+
+    private void AttachArrow(){
+        //Hey, Future me, this line is probably going to be the one
+        //That is horribly broken.
+        //I'm not exactly sure what the vareity of buttons for OVR are
+        //So, I'm going to copy pase someone's answer here until you solve it.
+        /*
+         * Returns true if the primary button (usually A) is pressed)
+         * OVRInput.Get(OVRInput.Button.One);
+         * 
+         * Returns true if the A button is pressesd on the Right controller
+         * OVRInput.Get(OVRInput.Button.One, OVRInput.Controller.RTouch);
+         * 
+         * To detect trigger presses
+         * (Returns float)
+         * OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger,OVRInput.Controller.LTouch);
+         */
+        if(!isAttached && OVRInput.Get(OVRInput.Button.One, OVRInput.Controller.RTouch)){
+            ArrowManager.Instance.AttachBowToArrow();
+            isAttached = true;
+        }
+    }
+}
---
+++ Alt-phys/Assets/BowArrow/Scripts/ArrowManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This code is more or less completely made by owner of FusedVR youtube channel
+//Updated by William Rindone to work with the Oculus SDK
+
+public class ArrowManager : MonoBehaviour
+{
+
+    public static ArrowManager Instance;
+
+    //Supposedly Going to track the remote
+    public OvrAvatarTouchController trackedObj;
+
+    private GameObject currentArrow;
+
+    public GameObject stringAttachPoint;
+
+    public GameObject arrowStartPoint;
+
+    public GameObject stringStartPoint;
+    
+    public GameObject arrowPrefab;
+
+    private bool isAttached = false;
+
+    private void Awake()
+    {
+        if(Instance == null){
+            Instance = this;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void Update()
+    {
+        AttachArrow();
+        PullString();
+    }
+
+    private void PullString(){
+        if(isAttached){
+            float dist = (stringStartPoint.transform.position - trackedObj.transform.position).magnitude;
+            stringAttachPoint.transform.localPosition = stringStartPoint.transform.localPosition + new Vector3(5f*dist, 0f, 0f);
+
+            //See Arrow for more on this thing...
+            if (OVRInput.GetUp(OVRInput.Button.One, OVRInput.Controller.RTouch))
+            {
+                Fire();
+            }
+        }
+    }
+
+    private void Fire(){
+        float dist = (stringStartPoint.transform.position - trackedObj.transform.position).magnitude;
+        currentArrow.transform.parent = null;
+        currentArrow.GetComponent<Arrow>().Fired();
+        Rigidbody r = currentArrow.GetComponent<Rigidbody>();
+        r.velocity = currentArrow.transform.forward * 10f * dist;
+        r.useGravity = true;
+
+        stringAttachPoint.transform.position = stringStartPoint.transform.position;
+
+        currentArrow = null;
+        isAttached = false;
+    }
+
+    private void AttachArrow(){
+        if(currentArrow == null){
+            currentArrow = Instantiate(arrowPrefab);
+            //Tracks the movement of the arrow to the movement of the remote
+            currentArrow.transform.parent = trackedObj.transform;
+            currentArrow.transform.localPosition = new Vector3(0f, 0f, .342f);
+            currentArrow.transform.localRotation = Quaternion.identity;
+        }
+    }
+
+    public void AttachBowToArrow(){
+        currentArrow.transform.parent = stringAttachPoint.transform;
+        currentArrow.transform.localPosition = arrowStartPoint.transform.localPosition;
+        currentArrow.transform.rotation = arrowStartPoint.transform.rotation;
+
+        isAttached = true;
+    }
+}
