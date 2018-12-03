98d6868e533653b5f9d3e4e41ce24608e831d024
WRindone <wrindone268@gmail.com>
2018-12-03 15:26:06 -0600

    Main Menu Update

---
+++ Alt-phys/Assets/Game/Scripts/MenuButton.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+
+    public UnityEngine.Object sceneToLoad;
+    public Material[] states;
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = states[0];
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        rend.sharedMaterial = states[1];
+        if (gameObject.CompareTag("QuitBox") && OVRInput.Get(OVRInput.Button.One, OVRInput.Controller.RTouch))
+        {
+            Application.Quit();
+        }
+        if (gameObject.CompareTag("TeleportBox") && OVRInput.Get(OVRInput.Button.One, OVRInput.Controller.RTouch))
+        {
+            SceneManager.LoadScene(sceneToLoad.name);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        rend.sharedMaterial = states[1];
+        if (gameObject.CompareTag("QuitBox") && OVRInput.Get(OVRInput.Button.One, OVRInput.Controller.RTouch))
+        {
+            Application.Quit();
+        }
+        if (gameObject.CompareTag("TeleportBox") && OVRInput.Get(OVRInput.Button.One, OVRInput.Controller.RTouch))
+        {
+            SceneManager.LoadScene(sceneToLoad.name);
+        }
+    }
+
+    private void OnTriggerExit()
+    {
+        rend.sharedMaterial = states[0];
+    }
+
+}
